590cb1a67a37afd4c0584df3428f808988630c43
f941033 <f941033@gmail.com>
2018-01-26 09:23:05 +0100

    Pantalla login inicial

---
+++ app/src/main/java/com/rcl/clase_di/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.rcl.clase_di;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
